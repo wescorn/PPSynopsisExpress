43b9a61ef270df6f7ebe8ea7630f1166e618b09b
hlynur92 <hlynur92@live.com>
2022-10-18 20:40:58 +0200

    Cleaned up Task Cancel

--- public/js/TaskCancelWorker.js
+++ public/js/TaskCancelWorker.js
@@ -2,11 +2,16 @@ onmessage = function (e) {
     console.log('Worker: Message received from main script');
     const message = e.data;
 
-    console.log('received', message);
-    postMessage("started working!");
-    Task();
-    
-    
+    console.log('Worker received data: ', message);
+    if (message == "Start Working") {
+        postMessage("Started Working!");
+        Task();
+    } else if (message == "Stop Working") {
+        //Clean up any work and stop the worker
+        postMessage("Stopping Working");
+        this.clearInterval();
+        this.close();
+    }
 }
 
 function Task() {
--- public/js/TaskCancellation.js
+++ public/js/TaskCancellation.js
@@ -1,56 +1,16 @@
 var m_worker = new Worker('js/TaskCancelWorker.js');
-    //new Worker('TaskCancelWorker.js')
 
-m_worker.postMessage("Running Loop...")
+m_worker.postMessage("Start Working")
 
 m_worker.onmessage = function (event) {
     console.log('worker returned data: ',event.data);
 };
 
 document.getElementById("cancelTaskBtn").addEventListener("click", function () {
-    m_worker.terminate()
-    console.log('terminated worker :)');
+    m_worker.postMessage("Stop Working")
+    console.log('Worker stopped');
 });
 
 m_worker.onerror = function (event) {
     console.log('Error in main', event.message);
 };
-
-
-/*
-function asyncThread(fn, ...args) {
-    if (!window.Worker) {
-        throw Promise.reject(new ReferenceError(`WebWorkers aren't available.`));
-    }
-    
-    const fnWorker = `
-  self.onmessage = function(message) {
-    (${fn.toString()})
-      .apply(null, message.data)
-      .then(result => self.postMessage(result));
-  }`;
-
-    return new Promise((resolve, reject) => {
-        try {
-            const blob = new Blob([fnWorker], { type: 'text/javascript' });
-            const blobUrl = window.URL.createObjectURL(blob);
-            const worker = new Worker(blobUrl);
-            window.URL.revokeObjectURL(blobUrl);
-
-            worker.onmessage = result => {
-                resolve(result.data);
-                worker.terminate();
-            };
-
-            worker.onerror = error => {
-                reject(error);
-                worker.terminate();
-            };
-
-            worker.postMessage(args);
-        } catch (error) {
-            reject(error);
-        }
-    });
-}
-*/
