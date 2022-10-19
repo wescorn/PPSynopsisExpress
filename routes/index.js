b5a5d3bf2b12a4198cfa6b31038bad7fc37921c6
Søren <act.wescorn@gmail.com>
2022-10-19 22:45:07 +0200

    tiny fix of little significance

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.setHeader('Cross-origin-Embedder-Policy', 'require-corp');
-  res.setHeader('Cross-origin-Opener-Policy','same-origin');
   res.render('index', { title: 'Express' });
 });
 
